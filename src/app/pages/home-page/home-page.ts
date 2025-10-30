12fd3e164cb9c1f9cff3e03193c086ff67ec9353
RansikaChandupa <ransikachandupa@gmail.com>
2025-10-30 13:14:34 +0530

    Offers section

--- src/app/pages/home-page/home-page.ts
+++ src/app/pages/home-page/home-page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainHeader} from './inner-items/main-header/main-header';
 import {OptionsComponent} from './inner-items/options-component/options-component';
+import {OffersComponent} from './inner-items/offers-component/offers-component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [MainHeader, OptionsComponent],
+  imports: [MainHeader, OptionsComponent, OffersComponent],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
